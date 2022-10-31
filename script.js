82f8151e073d144ebbd08fbdc027b3fb9ee68701
Lucas Sant'Ana <lucasbhribeiro01@gmail.com>
2022-10-31 16:22:07 -0300

    Novo botão html, nova função no js e estilização

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function confirmar() {
+async function digiAleatorioPorClasse() {
     var nivel = document.getElementById("nivelDigimon").value
     var API = `https://digimon-api.herokuapp.com/api/digimon/level/${nivel}`
 
@@ -15,7 +15,32 @@ async function confirmar() {
     var nome = document.createElement("p")
     nome.innerText = digimonAleatorio.name
     resultado.append(nome)
-    nome.classList.add("nome")
+    nome.classList.add("nomeElevel")
+
+    var imagem = digimonAleatorio.img
+    var img = document.createElement("img")
+    img.src = imagem
+    resultado.append(img)
+    img.classList.add("imagemDigimon")
+}
+
+async function digiAleatorio() {
+    var API = `https://digimon-api.herokuapp.com/api/digimon`
+
+
+    var response = await fetch(API)
+    var BodyJson = await response.json()
+    var numeroRandom = Math.floor(Math.random() * BodyJson.length)
+    var digimonAleatorio = BodyJson[numeroRandom]
+
+    var resultado = document.getElementById("resultado")
+
+    resultado.innerHTML = "";
+
+    var nome = document.createElement("p")
+    nome.innerText = digimonAleatorio.name
+    resultado.append(nome)
+    nome.classList.add("nomeElevel")
 
     var imagem = digimonAleatorio.img
     var img = document.createElement("img")
@@ -23,13 +48,10 @@ async function confirmar() {
     resultado.append(img)
     img.classList.add("imagemDigimon")
 
-   /*  var level = document.createElement("p")
+    var level = document.createElement("p")
     level.innerText = digimonAleatorio.level
     resultado.append(level)
-    level.classList.add("paragrafo") */
+    level.classList.add("nomeElevel")
 }
-
-
-
 
 
